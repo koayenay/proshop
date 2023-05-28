af12184bc5cba683d9f727e49bcff02a62ca71dd
koayenay <koayenay@gmail.com>
2023-05-28 13:47:21 -0700

    customer error and middleware setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose"
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      //   useUnifiedTopology: true,
-      //   useNewUrlParser: true,
-      //   createIndexes: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      // useCreateIndex: true, // This option is still valid
     })
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
-import products from "./data/products.js"
+import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import connectDB from "./config/db.js"
 
 dotenv.config()
@@ -10,18 +11,20 @@ connectDB()
 
 const app = express()
 
+// app.use((req, res, next) => {
+//   console.log(req.originalUrl)
+//   next()
+// })
+
 app.get("/", (req, res) => {
   res.send("API is running.....")
 })
 
-app.get("/api/products", (req, res) => {
-  res.json(products)
-})
+app.use("/api/products", productRoutes)
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
